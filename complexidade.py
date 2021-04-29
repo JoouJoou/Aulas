f681c2c5508d7c92430aa3aa5a98ab7c67b84e71
Rogerio Junior <rogerio.junior1016@gmail.com>
2021-04-28 23:11:37 -0300

    aulas

--- complexidade.py
+++ complexidade.py
@@ -9,13 +9,16 @@ soma = 0
 for i in codigo:
     if i.isalpha():
         codigo_final.append(i)
+    
     elif i.isnumeric():
         codigo_final.append(int(i))
 
 for i in range(len(codigo_final)):
+    
     if codigo_final[i] == "LOOP":
         loops.append(codigo_final[i])
         loops.append(codigo_final[i+1])
+    
     elif codigo_final[i] == "OP":
         if len(loops) == 0:
             custo+= codigo_final[i+1]
@@ -27,6 +30,7 @@ for i in range(len(codigo_final)):
             somaloop = codigo_final[i+1]
             somaloop*= loops[-1]
             soma+= somaloop
+    
     elif codigo_final[i] == "FIMLOOP":
         if len(loops) == 2:
             soma = soma*loops[-1]
